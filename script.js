c56acdd333785e679bbb8ae896ff3b9c8c999d2b
mdavio2404-web <mdavio2404@gmail.com>
2026-05-14 23:00:27 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// - Hero Slider
+
+// - Navbar Active Saat Scroll
+
+// Saat user scroll:
+// section “Menu” → navbar Menu aktif
+// section “Kontak” → navbar Kontak aktif
+// - Smooth Scroll Navigation
+// - Reveal Animation saat scroll
+// - Menu Filtering
+// - Modal Detail Menu
+// - Form Validation
+// - WhatsApp Order Generator
+// - Dark Mode
+// - Testimoni Slider
+// - Loading Animation
+// Saat website dibuka:
+// spinner muncul
+// lalu fade out
+// - Dynamic Menu dari Array/Object
+// - Search Menu
+// - Back To Top Button
+// Saat scroll muncul tombol:
+// ⬆️
+// - Hitung Total Pesanan
